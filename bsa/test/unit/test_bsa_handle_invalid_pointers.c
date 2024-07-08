80d1accbe2d2d2b9ee2368f4c689a746f1b6cbab
Егор Марков <e.markov@gt-sot.ru>
2024-07-08 22:09:06 +0300

    added test

--- bsa/test/unit/test_bsa_handle_invalid_pointers.c
+++ bsa/test/unit/test_bsa_handle_invalid_pointers.c
@@ -11,7 +11,17 @@ void test_new_bsa_hierarhy_handle_null() {
     ASSERT_EQ_PTR_NULL(hierarhy, "test_new_bsa_hierarhy_handle_null", LINE());
 }
 
+void test_new_bsa_handle_invalid_members() {
+    char *title = "title";
+    size_t members = -1;
+    H hierarhy = NULL;
+
+    hierarhy = new_bsa_hierarhy(title, members);
+    ASSERT_EQ_PTR_NULL(hierarhy, "test_new_bsa_handle_invalid_members", LINE());
+}
+
 int main() {
     test_new_bsa_hierarhy_handle_null();
+    test_new_bsa_handle_invalid_members();
     return 0;
 }
